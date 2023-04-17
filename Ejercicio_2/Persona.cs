f0a8728a800a9c631375ec394955ca2c9d1b2714
Pablo-notebook <juarezjpablo7@gmail.com>
2023-04-17 14:41:33 -0300

    cambios

--- Ejercicio_2/Persona.cs
+++ Ejercicio_2/Persona.cs
@@ -8,20 +8,84 @@ namespace Ejercicio_2
 {
     internal class Persona
     {
-        public string Nombre { get; set; }//Propiedad
+        public string Nombre { get; set; }//Propiedad  puedo dejarla de esta manera pero voy a declararla como atributo privado
         private DateTime FechaDeNacimiento //Propiedad
         {
             get { return _fechaDeNacimiento; }
-           // set { _fechaDeNacimiento = value; }  READONLY
+            set { _fechaDeNacimiento = value; }  //Si No declaro el set puedo hacerlo READONLY
         }
 
-        private int Dni 
-        { get { return _dni; }  }
+        private int Dni { get { return _dni; } set { _dni = value;  } }
         
         private DateTime _fechaDeNacimiento; //Atributo privado
+        
         private int _dni;                   //Atributo privado
+        private int _anio;
+        private int _mes;
+        private int _dia;
 
+        //   private string _nombre; 
 
+        /*
+         *A continuacion Dos Formas de notacion distintas, Ambas validas
+
+        set => _anio = value >1900 ? value:0;
+
+        set 
+        {
+            if (value>1900)
+            {
+                _anio = value;
+            }
+            else
+            {
+                _anio=0,
+                // trow new InvalidOperationException();
+            }
+         }
+         */
+        public Persona(string nombre, int anio,int mes,int dia, int dni)
+        {
+            Nombre = nombre;
+            _anio = anio;
+            _mes = mes;
+            _dia = dia;
+            _fechaDeNacimiento = new DateTime(_anio,_mes,_dia);
+            _dni = dni;
+        }
+
+        public string MostrarMayor()
+        {
+            string mensaje;
+            if (this.CalcularEdad() >= 18)
+            {
+                mensaje = "Es mayor";
+            }
+            else
+            {
+                mensaje= "Es menor";
+            }
+            return mensaje;
+        }
+        private int CalcularEdad()
+        {
+            int edad = ( 2023-this._anio);
+            // Console.WriteLine($"Edad: {edad}");
+            return edad;
+        }
+
+        public void Mostrar(string numero) 
+        {
+            StringBuilder sb= new StringBuilder();
+            sb.AppendLine($"--Persona {numero}");
+            sb.AppendLine($"Nombre: {Nombre}");
+            sb.AppendLine($"Fecha de Nacimiento: {FechaDeNacimiento}");
+            sb.AppendLine($"DNI: {Dni}");
+
+            sb.AppendLine($"Edad: {this.CalcularEdad()}");
+            sb.AppendLine($" {this.MostrarMayor()}");
+            Console.Write( sb );
+        }
 
     }
 }
--- Ejercicio_2/Program.cs
+++ Ejercicio_2/Program.cs
@@ -1,10 +1,51 @@
-﻿namespace Ejercicio_2
+﻿using System.Text;
+
+namespace Ejercicio_2
 {
     internal class Program
     {
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, World!");
+            Persona personaUno = new Persona("Pedro", 2000,4,5 , 34909999);
+            Persona personaDos = new Persona("Juan", 1990, 3, 8, 34555678);
+            Persona personaTres = new Persona("Ernesto", 2014, 5, 7, 36705444);
+          
+
+
+
+
+            StringBuilder xb = new StringBuilder();
+            xb.AppendLine("---MENU----");
+            xb.AppendLine("1 - Mostrar Fecha de Nacimiento");
+            xb.AppendLine("2 - Mostrar  ");
+            xb.AppendLine("3 -  ");
+            xb.AppendLine();
+            Console.WriteLine(xb);
+           
+            if (int.TryParse(Console.ReadLine(), out int opcion))
+                {
+                    switch (opcion)
+                    {
+                        case 1:
+                            
+                            break;
+                        case 2:
+                            
+                            personaUno.Mostrar("Uno");
+                            personaDos.Mostrar("Dos");
+                            personaTres.Mostrar("Tres");
+                            break;
+                        case 3:
+                            //Console.WriteLine("Ingrese el monto a retirar: ");
+                            //int.TryParse(Console.ReadLine(), out int retiro);
+                           // nuevaCuenta.retirar(retiro);
+                            break;
+
+                    }
+
+                }
+
         }
     }
 }
